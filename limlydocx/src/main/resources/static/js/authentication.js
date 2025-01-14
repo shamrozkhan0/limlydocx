52b5a984aefc1940bf3550d078c319255d29370c
youngs01 <124503790+youngs01@users.noreply.github.com>
2025-01-14 16:29:40 +0500

    Bug fixing , login page UI/UX , staticfile handling

---
+++ limlydocx/src/main/resources/static/js/authentication.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    console.log("i am ready ")
+
+    // Show password / hide password
+    $(".fa-eye-slash").click(function() {
+        let icon = $(this);
+        let passwordField = $("#password");
+    
+        if (passwordField.prop("type") === "password") {
+            passwordField.prop("type", "text"); 
+            icon.removeClass("fa-eye-slash").addClass("fa-eye");
+        } else {
+            passwordField.prop("type", "password"); 
+            icon.removeClass("fa-eye").addClass("fa-eye-slash");
+        }
+    });
+    
+
+    let input = document.querySelectorAll('input').forEach((text) => {
+        text.addEventListener("keypress", (e) => {
+            if (e.key === " ") {
+                e.preventDefault(); 
+            }
+        });
+    });
+    
+});
