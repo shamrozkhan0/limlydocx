7acb9dbace2c4dd6b884df57a2a98d486a5063e6
youngs01 <124503790+youngs01@users.noreply.github.com>
2025-05-12 23:03:01 +0500

    Working on an new strategy which reduces javascript and content of the editor in javascript

--- limlydocx/src/main/resources/static/js/editor.js
+++ limlydocx/src/main/resources/static/js/editor.js
@@ -1,1 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+
+
+    /**
+     * Auto hides the message after 5 seconds if present
+     */
+    function removesMessageIfPresent() {
+        const messageElement = document.querySelector(".message");
+        if (messageElement) {
+            console.log("Message detected. Auto-hide in 5s...");
+            setTimeout(() => {
+                messageElement.classList.add("d-none");
+                console.log("Message hidden.");
+            }, 5000);
+        }
+    }
+
+    removesMessageIfPresent();
+
+
+
+    });
